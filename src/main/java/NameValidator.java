48843b587f2d6f7376beaad5a574e1936d804867
Jack Duff <jackduff@Jacks-MacBook-Pro.local>
2025-12-13 16:29:47 +0000

    Update NameValidator

--- src/main/java/NameValidator.java
+++ src/main/java/NameValidator.java
@@ -1,6 +1,6 @@
-public class NameValidator{
+public class NameValidator {
 
-    public static boolean isValidName(String name){
-        return name != null && name.matches("[a-zA-z]+");
+    public static boolean isValidName(String name) {
+        return name != null && name.matches("[a-zA-Z]+");
     }
 }
