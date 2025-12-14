e0583c56521497ba46d086515a9c4bdec909208f
Jack Duff <jackduff@Jacks-MacBook-Pro.local>
2025-12-14 14:05:56 +0000

    remove test suite

--- src/main/java/NameValidator.java
+++ src/main/java/NameValidator.java
@@ -1,5 +1,5 @@
 public class NameValidator {
-
+    //class for name validator
     public static boolean isValidName(String name) {
         return name != null && name.matches("[a-zA-Z]+");
     }
